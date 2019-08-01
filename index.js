cc4bbb7f704ed04d9cb0ab88a1cefb50ca15c31c
codeasaglacier <remingtonshock@gmail.com>
2019-08-01 12:19:31 -0800

    fixed DOM manipulation, added hover effects

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const mouseOverLogo = document.querySelector('.copy');
-mouseOverLogo.forEach(element => {
-    element.addEventListener('mouseover', event => {          event.target.style.color = "white";
+const mouseOverNav = document.querySelector('.navBox');
+mouseOverNav.addEventListener('mouseover', (event) => {
+    event.target.style.color = "yellow";
+    setTimeout(() => event.target.style.color = "", 2000)
+});
+
+const mouseOverCopy = document.querySelectorAll('.copy');
+mouseOverCopy.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.style.color = "yellow";
+        setTimeout(() => event.target.style.color = "", 2000)
     })
 })
