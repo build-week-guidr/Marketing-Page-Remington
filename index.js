25fa2a0c2518ef879a4a70d66f17fb66986dccb6
codeasaglacier <remingtonshock@gmail.com>
2019-08-01 16:56:12 -0800

    added about page

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mouseOverNav = document.querySelector('.navBox');
 mouseOverNav.addEventListener('mouseover', (event) => {
     event.target.style.color = "yellow";
-    setTimeout(() => event.target.style.color = "", 2000)
+    setTimeout(() => event.target.style.color = "", 800)
 });
 
 const mouseOverCopy = document.querySelectorAll('.copy');
 mouseOverCopy.forEach(element => {
     element.addEventListener('mouseover', event => {
         event.target.style.color = "yellow";
-        setTimeout(() => event.target.style.color = "", 2000)
+        setTimeout(() => event.target.style.color = "", 800)
     })
 })
